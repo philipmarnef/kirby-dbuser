82f5fc025e9959c1422364f69147f150d437c33f
Adam Kiss <iam@adamkiss.com>
2024-03-19 22:46:51 +0100

    Add D in the cruD for users

--- html/site-app/site/config/config.php
+++ html/site-app/site/config/config.php
@@ -12,6 +12,7 @@ return [
 	'hooks' => [
 		'user.create:before' => fn(User $user, array $input) => CustomApp::db_user_create($input),
 		'user.update:before' => fn(User $user, array $values, array $strings) => CustomApp::db_user_update($user->id(), $values, $strings),
+		'user.delete:before' => fn(User $user) => CustomApp::db_user_delete($user),
 		'user.changeName:before' => fn(User $user, string $name) => CustomApp::db_user_update($user->id(), ['name' => $name]),
 		'user.changeEmail:before' => fn(User $user, string $email) => CustomApp::db_user_update($user->id(), ['email' => $email]),
 		'user.changePassword:before' => fn(User $user, string $password) => CustomApp::db_user_update($user->id(), ['password' => User::hashPassword($password)]),
